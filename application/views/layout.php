0134299d7ccedd234698ad2a45dc067820f78085
xbaxtian <xbaxtian@hotmail.com>
2018-04-17 09:14:21 -0500

    layout terminado

--- application/views/layout.php
+++ application/views/layout.php
@@ -5,15 +5,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  <html lang="es">
      <head>
          <meta charset="utf-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
          <title>Politiqueo</title>
          <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+         <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
+         <link rel="stylesheet" href="<?php echo base_url()?>css/style-layout.css">
      </head>
      <body>
         <header>
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-md-12">
-                        HEADER
+                    <div class="col-md-6">
+                        <h1><a class="home-link" href="<?php echo base_url()?>">POLITIQUEO</a></h1>
+                    </div>
+                    <div class="col-md-6">
+                        <form class="search-form">
+                            <div class="col-md-8 offset-md-4">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Busca tu politico..">
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-secondary" type="button">Go!</button>
+                                    </span>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -25,7 +40,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-12">
-                        FOOTER
+                        <button class="btn btn-default float-right">Quiero contribuir</button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 developed">
+                        LambdaDev - 2018
                     </div>
                 </div>
             </div>
